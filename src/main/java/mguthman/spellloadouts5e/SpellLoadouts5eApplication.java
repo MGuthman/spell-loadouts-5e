17ca0f2f12541ae73e369c4e039273027a9fd618
MAGuthman <mguthman@bellevue.edu>
2023-09-29 20:55:33 +0000

    Spring initial setup

---
+++ src/main/java/mguthman/spellloadouts5e/SpellLoadouts5eApplication.java
@@ -0,0 +1,13 @@
+package mguthman.spellloadouts5e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellLoadouts5eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellLoadouts5eApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mguthman/spellloadouts5e/SpellLoadouts5eApplicationTests.java
@@ -0,0 +1,13 @@
+package mguthman.spellloadouts5e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellLoadouts5eApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
